82a739006fb3c2dd25de9487ae606b51f0752e96
Bleu24 <bryanreamill2403@gmail.com>
2025-02-25 21:49:59 +0800

    added to toFixed(2) in displaying calories)

--- src/exercises/SummaryCard.jsx
+++ src/exercises/SummaryCard.jsx
@@ -2,7 +2,7 @@ const SummaryCard = ({ todayCalories }) => {
     return (
       <div className="bg-white shadow-lg rounded-lg p-6">
         <h2 className="text-xl font-semibold">Today's Burned Calories</h2>
-        <p className="text-3xl font-bold text-blue-600">{todayCalories} kcal</p>
+        <p className="text-3xl font-bold text-blue-600">{todayCalories.toFixed(2)} kcal</p>
       </div>
     );
   };
